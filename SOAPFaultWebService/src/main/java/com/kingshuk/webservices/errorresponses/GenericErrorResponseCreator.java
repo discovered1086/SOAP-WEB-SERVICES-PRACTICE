93680ca7d9048302966d954baf28768a050b176d
kimukher <kimukher@LNAR-93FRSF2.corp.capgemini.com>
2019-01-24 10:14:12 -0600

    Removing the interface

--- SOAPFaultWebService/src/main/java/com/kingshuk/webservices/errorresponses/GenericErrorResponseCreator.java
+++
@@ -1,8 +0,0 @@
-package com.kingshuk.webservices.errorresponses;
-
-@FunctionalInterface
-public interface GenericErrorResponseCreator<T extends PreEndpointErrorResponses> {
-	
-	public T getErrorResponse();
-
-}
